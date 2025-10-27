33ff81b14a66b08a42bd1c1f4eaf6b52eadd9ba9
Nadia Rodriguez <101356665+nadiazye@users.noreply.github.com>
2025-10-27 00:45:40 -0400

    beginning the project

---
+++ Dental Records/src/NZRDentalRecords.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; // import line needed in order to get user input
+public class NZRDentalRecords { // beginning of Dental Records class (includes my initials) =============================
+    //putting in Scanner keyboard ------------------------------------------
+    private static final Scanner keyboard = new Scanner(System.in);
+    //Scanner keyboard line ------------------------------------------------
+    // CONSTANT VARIABLES - DO NOT CHANGE -------------------------------------------------------------------------------
+    private static final int MAX_TEETH = 10;
+    private static final int MAX_TEETH_LOC = 2;
+    // CONSTANTS --------------------------------------------------------------------------------------------------------
+    public static void main(String[] args) { // beginning of main method ================================================
+
+    } // end of main method =============================================================================================
+} // end of Dental Records class (includes my initials) =================================================================
